3c96757739df7ff4f9252d15033635a0e90a10a4
Noic Noic <galanisd@csd.auth.gr>
2024-03-28 11:17:01 +0200

    added math function explanation

--- src/main/java/org/comp/tf_idf/TF_IDF.java
+++ src/main/java/org/comp/tf_idf/TF_IDF.java
@@ -5,6 +5,15 @@ import org.comp.item.Item;
 import java.util.HashMap;
 import java.util.List;
 
+
+/**
+ *
+ *  TF_DF(t,d,D) = tf(t,d) * idf (t,D)
+ *  tf(t,d) = n/N   is the term frequency, where n {@number of times term appear in document d} and N {@total_number of terms in document d}
+ *  idf(t,D) = log(N/n)   is the inverse document frequency, where N {@number of documentsin data set} and n{@number of documents that contain term in data set}
+ *
+ */
+
 public class TF_IDF {
 
     public TF_IDF() {}
